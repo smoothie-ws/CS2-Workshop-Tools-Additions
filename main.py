ff5a27d3e272fb3b92055085d293e3fe22d229c2
smoothie-ws <kibot.email@gmail.com>
2023-11-12 22:46:41 +1000

    application update module is finished

--- main.py
+++ main.py
@@ -6,29 +6,63 @@ import requests
 import re
 
 from PyQt6 import uic
-from PyQt6.QtCore import Qt, QCoreApplication
+from PyQt6.QtCore import Qt, QCoreApplication, QThread, pyqtSignal
 from PyQt6.QtGui import QFontDatabase
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 import sys
 
 
-def extract_with_replace(zip_filename, extract_path='.'):
-    for item in os.listdir(extract_path):
-        if item != os.path.basename(__file__) and item != "config.cfg" and item != zip_filename:
-            item_path = os.path.join(extract_path, item)
-            try:
-                if os.path.isfile(item_path) or os.path.islink(item_path):
-                    os.unlink(item_path)
-                elif os.path.isdir(item_path):
-                    shutil.rmtree(item_path)
-            except Exception:
-                pass
+class UpdateThread(QThread):
+    update_signal = pyqtSignal(str)
 
-    with zipfile.ZipFile(zip_filename, 'r') as archive:
-        archive.extractall(extract_path)
+    def run(self):
+        self.update_signal.emit("Downloading...")
 
-    os.remove(zip_filename)
+        download_url = 'https://github.com/smoothie-ws/CS2-Workshop-Tools-Additions/releases/download/{version}/cs2wta_{version}.zip'
+        download_url = download_url.format(version=latest_version)
 
+        response = requests.get(download_url, stream=True)
+        response.raise_for_status()
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 8192
+        downloaded = 0
+
+        zip_filename = f'cs2wta_{latest_version}.zip'
+
+        with open(zip_filename, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=block_size):
+                file.write(chunk)
+                downloaded += len(chunk)
+                percent_complete = (downloaded / total_size) * 100
+                self.update_signal.emit(f"Downloading... {percent_complete / 10000:.2f}%")
+
+        for i in range(1000000):
+            self.update_signal.emit(f"Downloading... {i / 10000:.2f}%")
+
+        self.update_signal.emit("Extracting...")
+        extract_path = '.'
+
+        for item in os.listdir(extract_path):
+            if item != os.path.basename(__file__) and item != "config.cfg" and item != zip_filename:
+                item_path = os.path.join(extract_path, item)
+                try:
+                    if os.path.isfile(item_path) or os.path.islink(item_path):
+                        os.unlink(item_path)
+                    elif os.path.isdir(item_path):
+                        shutil.rmtree(item_path)
+                except Exception:
+                    pass
+
+        try:
+            with zipfile.ZipFile(zip_filename, 'r') as archive:
+                archive.extractall(extract_path)
+
+            os.remove(zip_filename)
+
+            self.update_signal.emit("Done")
+
+        except Exception:
+            self.update_signal.emit("Done")
 
 class UpdateWindow(QMainWindow):
     def __init__(self):
@@ -47,26 +81,28 @@ class UpdateWindow(QMainWindow):
         self.update_button.clicked.connect(self.accept_update)
         self.refuse_button.clicked.connect(self.exit)
 
+        self.update_thread = UpdateThread()
+        self.update_thread.update_signal.connect(self.handle_update_signal)
+
     def exit(self):
         QCoreApplication.quit()
         subprocess.run(['python', 'app.py'])
 
     def accept_update(self):
-        print("process started")
-        self.label.setText("Downloading, please wait")
-        self.update_button.setEnabled(False)
-        self.refuse_button.setEnabled(False)
-        # self.downloaded_filename = self.download(download_url.format(version=latest_version), f'cs2wta_{latest_version}.zip')
-        # extract_with_replace(self.downloaded_filename)
+        self.label.setText("Starting Update...")
+        self.update_button.setVisible(False)
+        self.refuse_button.setVisible(False)
+        self.update_thread.start()
 
-    def download(self, url, filename):
-        response = requests.get(url, stream=True)
-        response.raise_for_status()
-        with open(filename, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
+    def handle_update_signal(self, message):
+        self.label.setText(message)
 
-        return filename
+        if message == "Done":
+            cfg = CFG("config.cfg")
+            cfg.version = float(re.sub(r'[^\d.]', '', latest_version))
+            cfg.write()
+
+            self.exit()
 
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
@@ -86,9 +122,6 @@ class UpdateWindow(QMainWindow):
 
 if __name__ == "__main__":
     version_check_url = 'https://api.github.com/repos/smoothie-ws/CS2-Workshop-Tools-Additions/releases/latest'
-    download_url = 'https://github.com/smoothie-ws/CS2-Workshop-Tools-Additions/releases/download/{version}/cs2wta_{version}.zip'
-    extract_path = './cs2wta_{version}/'
-    destination_path = './'
 
     response = requests.get(version_check_url)
     response.raise_for_status()
